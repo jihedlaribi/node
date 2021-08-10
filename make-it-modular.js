5dd3f8511720e18a395e9d12ed99da602bf75f6e
jihedlaribi <jihed.laribi13@gmail.com>
2021-08-10 14:29:44 +0100

    first

---
+++ make-it-modular.js
@@ -0,0 +1,15 @@
+var filterFn = require('./Mymoduler.js');
+var dir = process.argv[2];
+var filterStr = process.argv[3];
+
+filterFn(dir, filterStr, function (err, list) {
+    if (err) {
+        return console.error('There was an error:', err);
+    }
+
+    list.forEach(function (file) {
+        console.log(file)
+    });
+});
+
+
